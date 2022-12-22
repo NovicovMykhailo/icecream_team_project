dfdcbb36ddb3230147dc4a6076c78533d7f7d557
NovicovMykhailo <mikenovicov@gmail.com>
2022-12-22 13:48:48 +0200

    add media to gallery

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 // gallery Script
 $(function(){
-    setInterval(function(){
-       $(".slideshow ul").animate({marginLeft:-1013},2500,function(){
-          $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-       })
-    }, 5000);
- });
+   setInterval(function(){
+      $(".slideshow-mobile ul").animate({marginLeft:-280},2500,function(){
+         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+      })
+   }, 5000);
+});
+
+ $(function(){
+   setInterval(function(){
+      $(".slideshow-tablet ul").animate({marginLeft:-704},2500,function(){
+         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+      })
+   }, 5000);
+});
+
+$(function(){
+   setInterval(function(){
+      $(".slideshow-desktop ul").animate({marginLeft:-1013},2500,function(){
+         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+      })
+   }, 5000);
+});
