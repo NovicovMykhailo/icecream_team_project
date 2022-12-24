6ce0f183215adeb14679d2b2d781b2bddc067dab
NovicovMykhailo <mikenovicov@gmail.com>
2022-12-25 00:48:27 +0200

    product modals

--- src/js/modal-ingredients.js
+++ src/js/modal-ingredients.js
@@ -13,3 +13,33 @@
 		refs.modal.classList.toggle("is-hidden");
 	}
 })();
+(() => {
+	const refs = {
+		openModalBtn: document.querySelector("[ingredients-modal-open2]"),
+		closeModalBtn: document.querySelector("[ingredients-modal-close2]"),
+		modal: document.querySelector("[ingredients-modal2]"),
+	};
+
+	refs.openModalBtn.addEventListener("click", toggleModal);
+	refs.closeModalBtn.addEventListener("click", toggleModal);
+
+	function toggleModal() {
+		document.body.classList.toggle("modal-open");
+		refs.modal.classList.toggle("is-hidden");
+	}
+})();
+(() => {
+	const refs = {
+		openModalBtn: document.querySelector("[ingredients-modal-open3]"),
+		closeModalBtn: document.querySelector("[ingredients-modal-close3]"),
+		modal: document.querySelector("[ingredients-modal3]"),
+	};
+
+	refs.openModalBtn.addEventListener("click", toggleModal);
+	refs.closeModalBtn.addEventListener("click", toggleModal);
+
+	function toggleModal() {
+		document.body.classList.toggle("modal-open");
+		refs.modal.classList.toggle("is-hidden");
+	}
+})();
