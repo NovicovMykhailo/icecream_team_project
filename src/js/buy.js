a9c993915f664afd7f86b47ccab3f0f522bbf9d3
NovicovMykhailo <mikenovicov@gmail.com>
2022-12-25 22:27:58 +0200

    heder fix 3

--- src/js/buy.js
+++ src/js/buy.js
@@ -1,12 +1,17 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn2: document.querySelector(".data-modal-open2"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+ 
+  
+
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
