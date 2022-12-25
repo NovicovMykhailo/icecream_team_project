f0256cc43ffaa8a8088f57d2bbd6dcac7c56cd1d
HalynaSilina <halynasilina@gmail.com>
2022-12-25 18:29:07 +0200

    img

--- src/js/gallery.slider.js
+++ src/js/gallery.slider.js
@@ -1,23 +1,9 @@
-$(function(){
-   setInterval(function(){
-      $(".slideshow-mobile ul").animate({marginLeft:-280},2500,function(){
-         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-      })
-   }, 5000);
-});
-
- $(function(){
-   setInterval(function(){
-      $(".slideshow-tablet ul").animate({marginLeft:-704},2500,function(){
-         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-      })
-   }, 5000);
-});
-
-$(function(){
-   setInterval(function(){
-      $(".slideshow-desktop ul").animate({marginLeft:-1013},2500,function(){
-         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-      })
-   }, 5000);
-});
+ $(document).ready(function(){
+   $('.slideshow').slick({
+     easing: 'ease-in-out',
+      infinite: true,
+     autoplay: true,
+  speed: 600,
+  slidesToShow: 1
+      });
+    });
