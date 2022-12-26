d799086b82a883dc53abf5e7f4fb7f02f091066d
NovicovMykhailo <mikenovicov@gmail.com>
2022-12-26 02:40:48 +0200

    gallery_fix

--- src/js/gallery.slider.js
+++ src/js/gallery.slider.js
@@ -1,14 +1,3 @@
-//  $(document).ready(function(){
-//    $('.slideshow').slick({
-//      infinite: true,
-//      autoplay: true,
-//      autoplaySpeed: 5000,
-//      slidesToShow: 1,
-//      variableWidth: true,
-//      slidesToScroll: 1
-//       });
-//     });
-
 
 $(function(){
    setInterval(function(){
@@ -35,23 +24,3 @@ $(function(){
    }, 5000);
    
 });
-
-
-// $(function(){
-//   var timeSlide;
-//   function goSlide() {
-//       $(".slideshow__item").animate({marginLeft:'-300'},2500,function(){
-//         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-//       })
-
-//   }
-//   timeSlide = setInterval(goSlide, 5000);
-  
-//   $('.slideshow__item').on('mouseenter', function(){
-//     // stop animation
-//     clearInterval(timeSlide);
-//   }).on('mouseleave', function(){
-//     // play animation
-//     timeSlide = setInterval(goSlide, 3500);
-//   });
-// });
