de96c1793b0af0d5dc26b3087bcd7ffdcb5774b9
NovicovMykhailo <114993840+NovicovMykhailo@users.noreply.github.com>
2022-12-26 01:45:17 +0000

    Deploying to gh-pages from @ NovicovMykhailo/icecream_team_project@61b1d7dddbfb03c42c4dfa299676c918dd3f349c 🚀

--- index.cbefa5c1.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),openModalBtn2:document.querySelector(".data-modal-open2"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.openModalBtn2.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.cbefa5c1.js.map
